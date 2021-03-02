7ec550ea4564cb9c7c34c1aeba5eedb7ff935daf
Ubuntu <god@Zeus.nioszb5o4n3uxoabehoqi4y1rb.bx.internal.cloudapp.net>
2021-03-02 02:13:24 +0000

    Added scripts for 1,4,8 bit scalar quant on MRPC dataset

---
+++ roberta-mrpc-scalar-8-quant-noise.sh
@@ -0,0 +1,34 @@
+TOTAL_NUM_UPDATES=2296
+WARMUP_UPDATES=137
+LR=1e-05
+NUM_CLASSES=2
+MAX_SENTENCES=4
+ROBERTA_PATH=roberta_base/model.pt
+MRPC_PATH=MRPC-bin/
+SAVE_DIR=checkpoint/roberta/mrpc-scalar-8-quant-noise
+UPDATE_FREQ=4
+
+PYTHONPATH="~/Quant-Noisier/fairseq" python -m fairseq_cli.train $MRPC_PATH \
+    --restore-file $ROBERTA_PATH \
+    --max-positions 512 \
+    --batch-size $MAX_SENTENCES \
+    --max-tokens 4400 \
+    --task sentence_prediction \
+    --reset-optimizer --reset-dataloader --reset-meters \
+    --required-batch-size-multiple 1 \
+    --init-token 0 --separator-token 2 \
+    --arch roberta_base \
+    --criterion sentence_prediction \
+    --num-classes $NUM_CLASSES \
+    --dropout 0.1 --attention-dropout 0.1 \
+    --weight-decay 0.1 --optimizer adam --adam-betas "(0.9, 0.98)" --adam-eps 1e-06 \
+    --clip-norm 0.0 \
+    --lr-scheduler polynomial_decay --lr $LR --total-num-update $TOTAL_NUM_UPDATES --warmup-updates $WARMUP_UPDATES \
+    --max-epoch 10 \
+    --find-unused-parameters \
+    --best-checkpoint-metric accuracy --maximize-best-checkpoint-metric \
+    --ddp-backend legacy_ddp \
+    --quant-noise-scalar 0.5 \
+    --save-dir $SAVE_DIR \
+    --bits 8 \
+    --update-freq $UPDATE_FREQ
