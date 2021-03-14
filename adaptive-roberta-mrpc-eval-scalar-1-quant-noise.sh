e15eb221d63d1cae55274a2a8c15e1d23efd289b
Ubuntu <olympus@Poseidon.nioszb5o4n3uxoabehoqi4y1rb.bx.internal.cloudapp.net>
2021-03-14 03:52:56 +0000

    Add adaptive scripts for Quant-Noise for int 1, 4, 8 on MRPC

---
+++ adaptive-roberta-mrpc-eval-scalar-1-quant-noise.sh
@@ -0,0 +1,1 @@
+python mrpc_inference.py checkpoint/roberta/adaptive-mrpc-scalar-1-quant-noise checkpoint_best.pt  
---
+++ adaptive-roberta-mrpc-eval-scalar-4-quant-noise.sh
@@ -0,0 +1,1 @@
+python mrpc_inference.py checkpoint/roberta/adaptive-mrpc-scalar-4-quant-noise checkpoint_best.pt  
